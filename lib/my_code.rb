a18cf456259fb60a025e27da000788a44cd9e0ec
Connor Meth <connormeth27@gmail.com>
2019-09-13 17:41:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -7,7 +7,7 @@ map_to_negativize([1, 2, 3, -9])
 
 def map_to_no_change(array)
   dune = ["paul", "gurney", "vladimir", "jessica", "chani"]
-  array.map { |n| }
+  array.map { |n| n }
 end
 map_to_no_change(dune)
 
